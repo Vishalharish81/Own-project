7a179ff0c4c2c5dc92ae628e39306ec5647d7f8a
unknown <vishalharish81@gmail.com>
2023-10-10 20:48:00 +0530

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'project';
+export class AppComponent implements  OnInit {
+  constructor(private router:Router ,private route:ActivatedRoute,private _formBuilder: FormBuilder ){
+    this.router.navigate(['list'],{relativeTo: this.route});
+  
+  }
+  
+  ngOnInit(): void {
+  
+  }
+
 }
